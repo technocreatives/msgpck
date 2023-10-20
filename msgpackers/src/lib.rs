be77f84f4097c03b018fe37ebe3fe4574d108708
Joakim Hulthe <joakim@hulthe.net>
2023-10-20 11:16:12 +0200

    Fix doctests

--- msgpackers/src/lib.rs
+++ msgpackers/src/lib.rs
@@ -16,7 +16,7 @@ pub use piece::Piece;
 pub use rmp::Marker;
 pub use util::unpack_array_header;
 
-/// Trait for serializing a struct using msgpack.
+/// Trait for serializing a type using msgpack.
 pub trait MsgPack {
     type Iter<'a>: Iterator<Item = Piece<'a>>
     where
@@ -27,7 +27,7 @@ pub trait MsgPack {
     /// ```
     /// use msgpackers::MsgPack;
     /// let mut encoded = vec![];
-    /// for m in vec![0xDDu8, 0xEE, 3].encode() {
+    /// for m in vec![0xDDu8, 0xEE, 3].pack() {
     ///     encoded.extend_from_slice(m.as_bytes());
     /// }
     /// assert_eq!(encoded, [0x93, 0xCC, 0xDD, 0xCC, 0xEE, 3]);
@@ -35,16 +35,15 @@ pub trait MsgPack {
     fn pack(&self) -> Self::Iter<'_>;
 }
 
+/// Trait for deserializing a type using msgpack.
 pub trait MsgUnpack {
-    /// Returns an iterator of msgpack [Piece]s. Collect them all to produce a valid msgpack value.
+    /// Unpack a value from a msgpack bytes slice
     ///
     /// ```
-    /// use msgpackers::MsgPack;
-    /// let mut encoded = vec![];
-    /// for m in vec![0xDDu8, 0xEE, 3].encode() {
-    ///     encoded.extend_from_slice(m.as_bytes());
-    /// }
-    /// assert_eq!(encoded, [0x93, 0xCC, 0xDD, 0xCC, 0xEE, 3]);
+    /// use msgpackers::MsgUnpack;
+    /// let encoded = [0x93, 0xCC, 0xDD, 0xCC, 0xEE, 3];
+    /// let decoded: Vec<u8> = Vec::unpack(&mut &encoded[..]).unwrap();
+    /// assert_eq!(decoded, &[0xDDu8, 0xEE, 3]);
     /// ```
     fn unpack<'buf>(bytes: &mut &'buf [u8]) -> Result<Self, UnpackErr>
     where
