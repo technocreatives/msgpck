cc3fcb34fe59e960291aa6c2aa89ef4d17b032fe
Pascal Hertleif <pascal@technocreatives.com>
2023-10-30 12:04:28 +0100

    more complex tests with collections

--- msgpck_derive/tests/derive_nested.rs
+++ msgpck_derive/tests/derive_nested.rs
@@ -1,9 +1,10 @@
+use std::collections::{BTreeMap, HashMap};
+
 use msgpck::{MsgPck, UnMsgPck};
-use pretty_hex::*;
 use proptest::prelude::*;
 use proptest_derive::Arbitrary;
 
-#[derive(Arbitrary, Debug, PartialEq, MsgPck, UnMsgPck)]
+#[derive(Arbitrary, Debug, Clone, PartialEq, MsgPck, UnMsgPck)]
 struct Foo {
     a: bool,
     b: String,
@@ -13,6 +14,16 @@ struct Foo {
 struct Bar {
     a: f32,
     foo: Foo,
+    collection: Foo,
+}
+
+#[derive(Arbitrary, Debug, PartialEq, MsgPck, UnMsgPck)]
+struct Baz {
+    a: f32,
+    foo: Foo,
+    hashmap: HashMap<String, Vec<Foo>>,
+    btreemap: BTreeMap<u16, Foo>,
+    list: Vec<Foo>,
 }
 
 proptest! {
@@ -28,7 +39,23 @@ proptest! {
     fn roundtrip_bar(s: Bar) {
         let mut writer: Vec<_> = Vec::new();
         s.pack(&mut writer).unwrap();
+
+        // use pretty_hex::*;
+        // dbg!(writer.len(), &s, writer.hex_dump());
+
         let d = Bar::unpack(&mut &writer[..]).unwrap();
+        assert_eq!(s, d);
+    }
+
+    #[test]
+    fn roundtrip_baz(s: Baz) {
+        let mut writer: Vec<_> = Vec::new();
+        s.pack(&mut writer).unwrap();
+
+        // use pretty_hex::*;
+        // dbg!(writer.len(), &s, writer.hex_dump());
+
+        let d = Baz::unpack(&mut &writer[..]).unwrap();
         assert_eq!(s, d);
     }
 
@@ -38,7 +65,9 @@ proptest! {
         let mut writer: Vec<_> = Vec::new();
         s.pack(&mut writer).unwrap();
 
+        // use pretty_hex::*;
         // dbg!(writer.len(), &s, writer.hex_dump(), &assumed_size);
+
         match assumed_size {
             msgpck::SizeHint { min: Some(min), max: Some(max) } => {
                 assert!(min <= writer.len());
